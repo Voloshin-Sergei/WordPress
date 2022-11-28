e632485990adb57ae3633ca27b53c0a207d67d2f
Voloshin Sergei <4voloshin@gmail.com>
2022-11-28 15:04:38 +0300

    feat: added sripts, styles and fonts

--- wp_theme/index.php
+++ wp_theme/index.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="/css/fontello.css" />
-    <link rel="stylesheet" href="/css/style.css" />
     <title>Hamidic</title>
     <?php wp_head(); ?>
   </head>
@@ -204,8 +202,6 @@
         <div class="footer__text">Copyright &copy; Kenan Hamidic. All rights reserved.</div>
       </div>
     </footer>
-    <script src="/js/jquery-3.6.1.min.js"></script>
-    <script src="/js/index.js"></script>
     <?php wp_footer(); ?>
   </body>
 </html>
